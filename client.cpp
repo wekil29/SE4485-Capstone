5c3cec377bbc700af0e10fd4888354455d503890
Anthony Martinez <amartin9791@gmail.com>
2022-10-27 19:04:41 -0500

    deleting unneeded part of clientMessageService function

--- client.cpp
+++ client.cpp
@@ -20,13 +20,10 @@ void ClientMessageService(int sockfd)
     char buff[MAX];
     int n;
     for (;;) {
-        bzero(buff, sizeof(buff));
         printf("Enter the string : ");
         n = 0;
         while ((buff[n++] = getchar()) != '\n')
             ;
-        write(sockfd, buff, sizeof(buff));
-        bzero(buff, sizeof(buff));
         read(sockfd, buff, sizeof(buff));
         printf("From Server : %s", buff);
         if ((strncmp(buff, "exit", 4)) == 0) {
