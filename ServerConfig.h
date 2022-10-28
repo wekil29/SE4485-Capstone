fd036a69575fe23d817fe2f8444d0a91cbf81c32
acerjagrup <acerjagrup@gmail.com>
2022-10-27 20:16:08 -0500

    Added .config, fixed errors

--- ServerConfig.h
+++ ServerConfig.h
@@ -14,24 +14,21 @@ class ServerConfig
 {
    private:
       int port_num;
-      String ip_addr;
+      std::string ip_addr;
    
-      std::fstream readFile(String file_name) { return std::fstream(file_name, std::fstream::in); }
+      std::fstream readFile(std::string file_name) { return std::fstream(file_name, std::fstream::in); }
    
    public:
-      ServerConfig ServerConfig(String file_name) 
+      ServerConfig(std::string file_name)
       {
           std::fstream fs;
-          String port;
+          std::string port;
           fs = readFile(file_name);
           std::getline(fs, port);
-          std::getline(fs, ip_addr);
           fs.close();
-          port_num = std::stoi (port, std::string::size_type);
+          port_num = std::atoi (port.c_str());
       }
       
       void setPortNum(int num) { port_num = num; }
-      void setIPAddr(String address) { ip_addr = address; }
       int getPortNum() { return port_num; }
-      String getIPAddr() { return ip_addr; }
 };
--- server.cpp
+++ server.cpp
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "ServerConfig.h"
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
@@ -14,89 +15,72 @@ int sockfd, connfd;
 unsigned int len;
 struct sockaddr_in servaddr, cli;
 
-// Function designed for chat between client and server.
+int bindSocket(int port_num);
+
+// Function designed for chat between client and server
 void ServerMessageService(int connfd)
 {
-    char buff[MAX];
-    int n;
-    // infinite loop for chat
-    for (;;) {
-        bzero(buff, MAX);
-   
-        // read the message from client and copy it in buffer
-        read(connfd, buff, sizeof(buff));
-        // print buffer which contains the client contents
-        printf("From client: %s\t To client : ", buff);
-        bzero(buff, MAX);
-        n = 0;
-        // copy server message in the buffer
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-   
-        // and send that buffer to client
-        write(connfd, buff, sizeof(buff));
-   
-        // if msg contains "Exit" then server exit and chat ended.
-        if (strncmp("exit", buff, 4) == 0) {
-            printf("Server Exit...\n");
-            break;
-        }
-    }
+    // char buff[MAX];
+    char message[] = "This is a message from the server";
+
+    // Send a message to the client
+    write(connfd, message, sizeof(message));
 }
- void bindSocket();  
+
 // Driver function
 int main()
 {
-    
-   
-    // socket create and verification
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
-        printf("socket creation failed...\n");
-        exit(0);
-    }
-    else
-        printf("Socket successfully created..\n");
-    bzero(&servaddr, sizeof(servaddr));
-   
+    int connfd;
     // assign IP, PORT
+    std::string file_name = ".config";
+    ServerConfig serverConfig(file_name);
+    //serverConfig = ServerConfig(file_name);
+    int port_num = serverConfig.getPortNum();
+
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(PORT);
+    servaddr.sin_port = htons(port_num);
    
-    bindSocket();
-    len = sizeof(cli);
-   
-    // Accept the data packet from client and verification
-    connfd = accept(sockfd, (SA*)&cli, &len);
-    if (connfd < 0) {
-        printf("server accept failed...\n");
-        exit(0);
-    }
-    else
-        printf("server accept the client...\n");
-   
+    connfd = bindSocket(port_num);
+
     // Function for chatting between client and server
     ServerMessageService(connfd);
    
     // After chatting close the socket
     close(sockfd);
+    return 0;
 }
-void bindSocket(){
+int bindSocket(int port_num)
+{
+    int sockfd, connfd, length;
+    // Socket creation and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("Failed to create socket\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created\n");
+    bzero(&servaddr, sizeof(servaddr));
+
     // Binding newly created socket to given IP and verification
     struct sockaddr_in servaddr, cli;
     if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-        printf("socket bind failed...\n");
+        printf("Failed to bind socket\n");
         exit(0);
     }
     else
-        printf("Socket successfully binded..\n");
-   
-    // Now server is ready to listen and verification
-    if ((listen(sockfd, 5)) != 0) {
-        printf("Listen failed...\n");
+        printf("Socket successfully bound\n");
+
+    // Accept the data packet from client and verification
+    len = sizeof(cli);
+    connfd = accept(sockfd, (SA*)&cli, &len);
+    if (connfd < 0) {
+        printf("Failed to accept client\n");
         exit(0);
     }
     else
-        printf("Server listening..\n");
+        printf("Client accepted by server\n");
+
+    return connfd;
 }
