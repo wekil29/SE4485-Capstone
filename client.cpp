b94b1849a7da52350aea866fdbeee9d986e594c6
jnj190001 <71573799+jnj190001@users.noreply.github.com>
2022-11-18 14:22:30 -0600

    Update client.cpp with seccomp changes
        summary:    seccomp functionality added for client. installation and demo of seccomp filter
    
        changes:    include statements for seccomp.h and seccomp-bpf.h added. arguments for main added. 
                    The seccomp portion (line 77+) was added.

--- client.cpp
+++ client.cpp
@@ -1,6 +1,18 @@
+/*
+    File updated on 11/18/22 by Justin Jones. 
+
+    summary:    seccomp functionality added for client. installation and demo of seccomp filter
+
+    changes:    include statements for seccomp.h and seccomp-bpf.h added. arguments for main added. 
+                The seccomp portion (line 77+) was added.
+*/
+
+
 #include <arpa/inet.h> // inet_addr()
 #include <strings.h> // bzero()
 #include "Config.h"
+#include "seccomp.h"
+#include "seccomp-bpf.h"
 
 #define MAX 200
 #define IP_ADDR "127.0.0.1"
@@ -44,7 +56,7 @@ int connectSocket(int port_num)
     return sockfd;
 }
 
-int main()
+int main(int argc, char** argv)
 {
     // Get the socket
     char IP_ADDRESS[] = "127.0.0.1";
@@ -61,4 +73,48 @@ int main()
     // Close the socket
     printf("Closing client socket\n");
     close(sockfd);
+
+    //seccomp
+    int before_allow = 1;
+    int after_allow = 1;
+
+    signal(SIGSYS, handle_sigsys);
+    parse_args(argc, argv, &before_allow, &after_allow);
+
+    if (install_syscall_filter( before_allow )) 
+    {
+        printf("filter install failure: original\n");
+        exit(4);
+    }
+
+    pid_t forkStatus;
+    forkStatus = fork();
+
+    if (forkStatus == 0) 
+    {
+        signal(SIGSYS, handle_sigsys);
+        printf("-->  Hello from the child. Installing filter now.\n");
+        if (install_syscall_filter( after_allow )) {
+            printf("filter install failure: child\n");
+            exit(5);
+        }
+        printf("-->  Child sleeping.\n");
+        sleep(2);
+        printf("-->  Can child get pid?.\n");
+        pid_t cpid = getpid();
+        printf("-->  childpid = %d.\n", cpid);
+        printf("-->  Child terminating.\n");
+    }
+    else if (forkStatus != -1) 
+    {
+        printf("[Parent process.]\n");
+        wait(NULL);
+        pid_t ppid = getpid();
+        printf("[as the parent I can get my PID. See pid=%d.]\n", ppid);
+        printf("[Parent terminating]\n");
+
+    } else {
+        perror("Error calling fork()");
+    }
+
 }
