68c1ed3822ca175fa41017835e781e8cf1c747cd
JeremiahTrinh <89665166+JeremiahTrinh@users.noreply.github.com>
2022-11-22 18:15:45 -0600

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,9 +1,9 @@
 /*
-    File updated on 11/18/22 by Justin Jones. 
+    File updated on 11/18/22 by Justin Jones.
 
     summary:    seccomp functionality added for client. installation and demo of seccomp filter
 
-    changes:    include statements for seccomp.h and seccomp-bpf.h added. arguments for main added. 
+    changes:    include statements for seccomp.h and seccomp-bpf.h added. arguments for main added.
                 The seccomp portion (line 77+) was added.
 */
 
@@ -56,11 +56,17 @@ int connectSocket(int port_num)
     return sockfd;
 }
 
-int main(int argc, char** argv)
+void set_seccomp_filters()
+{
+   install_filter(__NR_open, AUDIT_ARCH_X86_64, EPERM);
+   // Add more syscall filters below as needed
+}
+
+int main()
 {
     // Setting up variables
-    int before_allow = 1;   // seccomp variable 
-    int after_allow = 1;    // seccomp variable
+    //int before_allow = 1;   // seccomp variable
+    //int after_allow = 1;    // seccomp variable
     char IP_ADDRESS[] = "127.0.0.1";    // Get the socket
     std::string file_name = ".config";  // Name of config file with port number
     int port_num;                       // Port number
@@ -68,22 +74,28 @@ int main(int argc, char** argv)
 
     // Get port
     Config config(file_name);
-    port_num = config.getPortNum();
+    port_num = config.getPortNum(); // Must run before seccomp filter is installed so that it does not get blocked
 
     //seccomp
-    signal(SIGSYS, handle_sigsys);
-    parse_args(argc, argv, &before_allow, &after_allow);
-    if (install_syscall_filter(before_allow)) 
-    {
-        printf("filter install failure\n");
-        exit(4);
-    }
-    
+    set_seccomp_filters();
+
     sockfd = connectSocket(port_num);
     // Receive a message from the server
-    getMessage(sockfd);
- 
+
+        getMessage(sockfd);
+
     // Close the socket
     printf("Closing client socket\n");
     close(sockfd);
+
+    // Attempt the restricted syscall (open)
+    std::string restricted_file_name = "seccomp.h";
+    std::string restricted_result = "open syscall was blocked."; // Expected: Should remain unchanged since overriding reads are blocked by seccomp
+    std::fstream testStream (restricted_file_name, std::fstream::in);
+    // Attempt to read 5th line from file; should be blocked by seccomp
+    for (int i = 0; i < 5; i++)
+    {
+       std::getline (testStream, restricted_result);
+    }
+    printf("Result of restricted syscall: %s \n", restricted_result.c_str());
 }
